4b56b1013ac42a212a0882d59fb23838376511e8
Geoffrey Wright <Geoff@MacBook-Pro-2.local>
2015-12-30 22:24:46 -0500

    hot reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
 var path = require('path');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
+var merge = require('webpack-merge');
+var webpack = require('webpack');
 
+
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+
+const common = {
+
   // Entry accepts a path or an object of entries.
   // The build chapter contains an example of the latter.
   entry: PATHS.app,
@@ -20,3 +26,32 @@ module.exports = {
     })
   ]
 };
+
+
+// Default configuration
+if(TARGET === 'start' || !TARGET) {
+
+  module.exports = merge(common, {
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      // Parse host and port from env so this is easy to customize.
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+
+}
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {});
+}
